15e95bd7f23c420ebd663864d3d87a3849634c80
Sohee Kim <kim66s@mtholyoke.edu>
2020-12-31 00:45:33 +0900

    add basic menu widget ui

--- lib/widgets/menu/menu_widget.dart
+++ lib/widgets/menu/menu_widget.dart
@@ -1,16 +1,41 @@
 import 'package:flutter/material.dart';
 
-class MenuWidget extends StatefulWidget {
-  @override
-  _MenuWidgetState createState() => _MenuWidgetState();
-}
-
-class _MenuWidgetState extends State<MenuWidget> {
+class MenuWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blue,
       width: MediaQuery.of(context).size.width * 0.04,
+      child: Column(children: [
+        Container(
+            width: MediaQuery.of(context).size.width * 0.04,
+            decoration: const BoxDecoration(border: Border(left: BorderSide(width: 5.0, color: Colors.black))),
+            child: selectedPaddingIcon(Icons.file_copy_outlined)),
+        paddingIcon(Icons.search_sharp),
+        paddingIcon(Icons.apps),
+        paddingIcon(Icons.tag_faces_outlined)
+      ]),
+    );
+  }
+
+  Padding paddingIcon(IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Icon(
+        icon,
+        color: Colors.grey[850],
+        size: 35.0,
+      ),
+    );
+  }
+
+  Padding selectedPaddingIcon(IconData icon) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Icon(
+        icon,
+        size: 35.0,
+      ),
     );
   }
 }
