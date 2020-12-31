23f78a2558172f0353a776ca853cca936e9db7d0
Sohee Kim <kim66s@mtholyoke.edu>
2020-12-31 14:34:03 +0900

    show blank bottom bar if screen is small

--- lib/widgets/bottom_widget.dart
+++ lib/widgets/bottom_widget.dart
@@ -3,17 +3,16 @@ import 'package:flutter/material.dart';
 class BottomWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
     return Container(
-      height: MediaQuery.of(context).size.height * 0.04,
-      color: Colors.green,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          // TODO(Sohee): dummy text
-          children: const [Text('Sohee Porfolio'), Text('made in 2021')],
-        ),
-      ),
-    );
+        height: height * 0.04,
+        color: Colors.green,
+        child: height > 430
+            ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // TODO(Sohee): dummy text
+                    children: const [Text('Sohee Porfolio'), Text('made in 2021')]))
+            : null);
   }
 }
